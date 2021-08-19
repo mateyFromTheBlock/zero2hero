4a778e97a3aa82858cf11c6d575df3fef6ed8e21
Matan Navon <matann@tamuzgroup.co.il>
2021-08-19 19:17:32 +0300

    Changing redirect url

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const providerConfig = {
   domain: config.domain,
   clientId: config.clientId,
   ...(config.audience ? { audience: config.audience } : null),
-  redirectUri: 'https://master--modest-gates-abde4a.netlify.app/history',
+  redirectUri: `${window.location.origin}/history`,
   onRedirectCallback,
 };
 
